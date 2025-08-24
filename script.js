33a0509f1ebd237f544c3918ac48101b1fa4c88c
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-08-25 01:50:01 +0600

    directed to another page

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 document.getElementById('loginButton').addEventListener('click', function(e){ 
     e.preventDefault()
-    console.log('This button has been clicked')
-    console.log(e)
+    const mobileNUmber = 12345678910
+    const pinNumber = 1234
+    
+    const mobileNUmberValue = document.getElementById('mobile-number').value
+    const mobileNUmberValueConverted = parseInt(mobileNUmberValue)
+
+    const pinNumberValue = document.getElementById('pin-number').value
+    const pinNumberConverted = parseInt(pinNumberValue)
+
+    console.log(mobileNUmberValueConverted, pinNumberConverted)
+
+    if(mobileNUmberValueConverted === mobileNUmber && pinNumberConverted === pinNumber){
+        window.location.href="./home.html"
+    }
+    else{
+        alert('Invalid Credentials')
+    }
 })
