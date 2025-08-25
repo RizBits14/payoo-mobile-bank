06de7cdc6d860c8047550f3bd80510e75a7d884f
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-08-25 23:55:33 +0600

    validation of pin and a/c number

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const validPin = 1234
+
 document.getElementById('add-money-btn').addEventListener('click', function(e){ 
     e.preventDefault()
     const bank = document.getElementById('bank').value
@@ -6,9 +8,19 @@ document.getElementById('add-money-btn').addEventListener('click', function(e){
     
     const amount = parseInt(document.getElementById('add-amount').value)
     
-    const pin = document.getElementById('add-pin').value
+    const pin = parseInt(document.getElementById('add-pin').value)
 
     const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+
+    if(accountNumber.length < 11){
+        alert('Please provide valid account number')
+        return
+    }
+
+    if (pin !== validPin){
+        alert('Please provide a valid pin number')
+        return
+    }
 
     const totalNewAvailableBalance = amount + availableBalance
 
